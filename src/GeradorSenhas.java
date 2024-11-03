96ecd3c9c8690ecce92d9918fef6ad6a72ab71bd
nivoningles <contadonivon@gmail.com>
2024-11-02 22:50:25 -0300

    Primeiro commit, ainda tem alguns erros para ajeitar

--- src/GeradorSenhas.java
+++ src/GeradorSenhas.java
@@ -1,4 +1,49 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class GeradorSenhas {
+    private JPanel panelInicial;
+    private JLabel txtGeradorDeSenhas;
+    private JButton btnGerador;
+    private JTextField txtSenhaGerada;
+    private JButton btnLimpar;
+    private JTextField txtNumeroCarac;
+
+    SenhaAleatoria aleatoria = new SenhaAleatoria();
+    String senhaGerada = "";
+    String numeroAleatorio = "";
+
+
+    public GeradorSenhas() {
+
+        btnGerador.addActionListener(new ActionListener() {
+
+             public void actionPerformed(ActionEvent e) {
+
+                try{
+                    numeroAleatorio = (aleatoria.gerarSenha(Integer.valueOf((txtNumeroCarac.getText()))));
+                    txtSenhaGerada.setText(numeroAleatorio);
+                }catch (Exception ex){
+                    txtSenhaGerada.setText("Informe um valor numerico");
+                }
+
+
+
+
+             }
+
+        });
+    }
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Minha Calculadora");
+        frame.setContentPane(new GeradorSenhas().panelInicial);
+        frame.setSize(400,400);
+        frame.setVisible(true);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
 }
--- src/SenhaAleatoria.java
+++ src/SenhaAleatoria.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 public class SenhaAleatoria {
+
+    private ArrayList<String> resposta = new ArrayList<>() ;
+    private String respostaFinal = "";
+    Random gerador = new Random();
+
+    String[] letrasMaiusculas = {"A","B","C","D","E","F","G","H","I","J","K","L","M","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    String[] letrasMinusculas = {"a","b","c","d","e","f","g","h","i","j","k","l","m","o","p","q","r","s","t","u","v","w","x","y","z"};
+    String[] caracteresEspeciais = {".", "@", "!", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "-", "=", "{", "}", "[", "]", "|", ":", ";", "'", "<", ">", ",", ".", "?", "/", "~", "`"};
+    String[] numerosGerados = {"1","2","3","4","5","6","7","8","9","0"};
+
+
+
+    public String gerarSenha (Integer numero) {
+
+        if(numero < 5){
+            return "Informe um numero maior que 4 caracteres";
+        } else {
+
+            this.respostaFinal = this.respostaFinal + this.letrasMaiusculas[gerador.nextInt(24)];
+            this.respostaFinal = this.respostaFinal + this.letrasMinusculas[gerador.nextInt(24)];
+            this.respostaFinal = this.respostaFinal + this.caracteresEspeciais[gerador.nextInt(30)];
+            this.respostaFinal = this.respostaFinal + this.numerosGerados[gerador.nextInt(9)];
+            this.respostaFinal = this.respostaFinal + this.letrasMaiusculas[gerador.nextInt(24)];
+
+
+            return this.respostaFinal.toString();
+
+        }
+
+
+    }
+
+
 }
